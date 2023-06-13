6279bf70823c80808329270213a6e972008dea93
mtq11 <mtquyen1002@gmail.com>
2023-06-13 21:32:15 +0700

    Up load source 1st

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import 'swiper/swiper.min.css';
+import './App.scss';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Catalog from './pages/Catalog'
+import Detail from './pages/detail/Detail';
+import Searchlog from './pages/search/Searchlog';
+import Home from './pages/Home';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/search/:keyword' element={<Searchlog/>}/>
+        <Route extact path="/:category" element={<Catalog/>}/>
+        <Route extact path="/:category/:id" element={<Detail/>}/>
+        <Route extact path="/" element={<Home/>}/>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
